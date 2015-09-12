b8c266d20be367d8576fcd8629972fe519f1df8d
crodociles <lukefroud86@gmail.com>
2015-09-12 23:29:51 +0100

    update 12/09/2015

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#services").mouseover(function(){
+    	$("#services_dd").css("display","block");
+    });
+    $("#services").mouseout(function(){
+    	$("#services_dd").css("display","none");
+    });
+});
